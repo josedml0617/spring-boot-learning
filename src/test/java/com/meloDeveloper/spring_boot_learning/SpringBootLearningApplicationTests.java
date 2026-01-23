3c084ed924d2120287c40fee8445429ceda742ec
José Marcos <josemarcosdemelolima1@gmail.com>
2026-01-23 09:48:22 -0300

    Create Project

---
+++ src/test/java/com/meloDeveloper/spring_boot_learning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meloDeveloper.spring_boot_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
