d3ed2365b3181f01d4eb6212066abf77a3e3dbb8
José Marcos <josemarcosdemelolima1@gmail.com>
2026-02-04 17:22:49 -0300

    Exception Handing -Delete

--- src/main/java/com/meloDeveloper/spring_boot_learning/services/UserService.java
+++ src/main/java/com/meloDeveloper/spring_boot_learning/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.meloDeveloper.spring_boot_learning.entities.User;
 import com.meloDeveloper.spring_boot_learning.repositories.UserRepository;
+import com.meloDeveloper.spring_boot_learning.services.exceptions.DatabaseException;
 import com.meloDeveloper.spring_boot_learning.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +32,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+		repository.deleteById(id);}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public User update(Long id, User obj) {
---
+++ src/main/java/com/meloDeveloper/spring_boot_learning/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.meloDeveloper.spring_boot_learning.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException (String msg) {
+		super(msg);
+	}
+}
